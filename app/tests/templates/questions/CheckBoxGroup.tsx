4cdc6de45abca2b294e09038893d057586d11a23
thienphuc-dev <phucnguyenvuong07@gmail.com>
2023-08-29 13:38:25 +0700

    flatlist? meh...

--- app/tests/templates/questions/CheckBoxGroup.tsx
+++ app/tests/templates/questions/CheckBoxGroup.tsx
@@ -1,6 +1,6 @@
 import CheckBox from "@react-native-community/checkbox";
 import { useEffect, useRef, useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native-windows";
+import { StyleSheet, Text, View } from "react-native-windows";
 
 const styles = StyleSheet.create({
     option: {
@@ -13,6 +13,10 @@ const styles = StyleSheet.create({
         transform: [{ scaleX: 0.7 }, { scaleY: 0.7 }],
         display: "flex",
         justifyContent: "flex-start"
+    },
+    wrapper: {
+        display: "flex",
+        flexDirection: "column"
     }
 })
 
@@ -22,6 +26,7 @@ const CheckBoxGroup = (props: any): JSX.Element => {
 
     const [optionsAnswers, _] = useState(Array(props.options?.length && 0).fill(false));
     const checked = useRef(0);
+    const f = useRef(Array<JSX.Element>());
 
     useEffect(() => {
         _(Array(props.options?.length).fill(false))
@@ -47,34 +52,39 @@ const CheckBoxGroup = (props: any): JSX.Element => {
 
     }
 
+    const renderOptions = () => {
+
+        const l = props.options.length;
+        f.current = Array<any>();
+        for (let i = 0; i < l; i++) {
+            f.current.push((
+                <View style={styles.option} key={i}>
+                    <CheckBox
+                        value={optionsAnswers[i]}
+                        onValueChange={(v) => changeOption(v, i)}
+                        style={styles.checkbox}
+                        onFillColor="#767676"
+
+                    />
+                    <Text style={{
+                        fontFamily: 'Arial,Helvetica,sans-serif',
+                        fontSize: 14
+                    }} selectable>{props.options[i]}</Text>
+                </View>
+            ))
+        }
+
+        return f.current;
+
+    }
+
 
     return (
-        <View>
-            <FlatList
-                data={props.options}
-                renderItem={
-                    ({ item, index }) => {
+        <View style={styles.wrapper}>
 
-                        return (
-                            <View style={styles.option}>
-                                <CheckBox
-                                    value={optionsAnswers[index]}
-                                    onValueChange={(v) => changeOption(v, index)}
-                                    style={styles.checkbox}
-
-                                />
-                                <Text style={{
-                                    fontFamily: 'Arial,Helvetica,sans-serif',
-                                    fontSize: 14
-                                }} selectable>{item}</Text>
-                            </View>
-                        );
-                    }
-
-                }
-                keyExtractor={item => item}
-            />
-
+            {
+                renderOptions()
+            }
         </View>
     );
 }
